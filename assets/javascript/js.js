9de7fb843e146f287cf200986944daf35c4ce9ef
alinavo <alinahuynhvo@gmail.com>
2018-12-14 16:47:59 -0500

    most of code works but not API key

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -1,42 +1,89 @@
-//API information
-//API Key: QuYYKFCfRPq0DkGMChE0k7owDdvOSSj6
-
 
 //array for topics
-var topics = ["Harry", "Ron", "Hermione", "Dumbledore", "Neville", "Ginny", "Voldemort"];
+var topics = ["Harry Potter", "Ron Weasley", "Hermion Granger", "Albut Dumbledore", "Neville Longbottom", "Ginny Weasley", "Lord Voldemort"];
 console.log(topics);
 
-
-//function that makes buttons
-function renderButtons() {
-    // empty divs so there are no repeats when new topics are added
-    $("#buttons-display").empty();
-    //loop that makes buttons       
-for (var i = 0; i < topics.length; i++) {
-    // Then dynamicaly generates buttons for each element in array
-    // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-    var a = $("<button>");
-    // Adds a class of movie to our button
-    // a.addClass("movie");
-    // Added a data-attribute
-    // a.attr("data-name", topics[i]);
-    // Provided the initial button text
-    a.text(topics[i]);
-    // Added the button to the buttons-view div
-    $("#buttons-display").append(a);
-    console.log(a);
-}};
-
-    $("#add-topic").on("click", function(event) {
-        event.preventDefault();
-        // This line of code will grab the input from the textbox
-        var newTopic = $("#topic-input").val().trim();
-        console.log(newTopic);
-        // Pushes new button topic created into topics array
-        topics.push(newTopic);
-        console.log(topics);
+function getButtons() {
+    $(".buttons-display").empty();
+    for (var i = 0; i < topics.length; i++) {
+        var button = $("<button>").text(topics[i]);
+        button.attr("data-name", topics[i]);
+        button.addClass("gif-button");
+        $("#buttons-display").append(button);
+        console.log(button);
+    }
+}
+getButtons();
 
 
-        renderButtons();
+//adding a new topic to empty array
+$(document).on('click', "#add-topic", function () {
+    event.preventDefault();
+    // This line of code will grab the input from the textbox
+    var newTopic = $("#topic-input").val().trim();
+    console.log(newTopic);
+    // Pushes new button topic created into topics array
+    topics.push(newTopic);
+    console.log(topics);
+    $("#topic-input").val('');
+    getButtons();
 });
 
+$(document).on("click", "button", function () {
+    // "this"= button that was clicked
+    var person = $(this).attr("data-gif");
+    // Constructing a URL to search Giphy for the name of the person who said the quote
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        person + "&api_key=QuYYKFCfRPq0DkGMChE0k7owDdvOSSj6&limit=10";
+    // Performing our AJAX GET request
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        // After the data comes back from the API
+        .done(function (response) {
+            // Storing an array of results in the results variable
+            var results = response.data;
+            console.log(results);
+
+            // Looping over every result item
+            for (var i = 0; i < results.length; i++) {
+
+                // Only taking action if the photo has an appropriate rating
+                if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+                    // Creating a div for the gif
+                    var gifDiv = $("<div>");
+                    // Storing the result item's rating
+                    var rating = results[i].rating;
+                    // Creating a paragraph tag with the result item's rating
+                    var p = $("<p>").text("Rating: " + rating);
+                    // Creating an image tag∫
+                    var gifImage = $("<img class = 'result'>");
+                    // Giving the image tag an src attribute of a proprty pulled off the
+                    // result item
+                    gifImage.attr("src", results[i].images.fixed_height.url);
+                    gifImage.attr("data-state", "still");
+                    gifImage.attr("data-still", results[i].images.fixed_height_still.url);
+                    gifImage.attr("data-animate", results[i].images.fixed_height.url);
+                    // Appending the paragraph and personImage we created to the "gifDiv" div we created
+                    gifDiv.append(p);
+                    gifDiv.append(gifImage);
+                    // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
+                    $("#gifs-display").append(gifDiv);
+                }
+            }
+        });
+
+    $(document).on("click", ".result", function () {
+        var state = $(this).attr("data-state");
+
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+        } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
+
+    });
+});
